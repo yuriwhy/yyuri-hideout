81fc14e6e0780792a99085141fa849db0c618df1
yyurᴉ <zaleatorio43@gmail.com>
2025-09-07 21:50:15 -0300

    Update script.js

--- pages/MaterialCalc/script/script.js
+++ pages/MaterialCalc/script/script.js
@@ -1,13 +1,4 @@
-document.body.style.zoom = "100%"; // Isso define o zoom padrão do site (aprendi sozinho e sem querer, descobri que pode ser que fique errado)
-
-/*
-    Antes eu tava esquecendo de colocar o ".value", por isso tava dando errado,
-    foi uma dor de cabeça pra eu lembrar que tinha que colocar isso kkkk
-
-    Um detalhe importante: Apartir da função de calcular o módulo, eu usei o Gemini (IA do Google)
-                           pra saber quais eram as operações matemáticas, mas eu não usei IA pra adaptar as operações no meu código,
-                           adaptei manualmente, porque é algo bem simples e fácil.
-*/
+document.body.style.zoom = "100%";
 
 function calc(operacao) {
   let n1 = Number(document.getElementById("num1").value);
@@ -50,57 +41,3 @@ function calc(operacao) {
   }
   document.getElementById("saida").innerHTML = "Resultado: " + res;
 }
-
-/*
-    Código Antigo:
-    
-function soma() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = n1 + n2
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function subt() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = n1 - n2
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function mult() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = n1 * n2
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function div() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = n1 / n2
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function mod() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = n1 % n2
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function pot() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = Math.pow(n1, n2)
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function raiz() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = Math.sqrt(n1)
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-function abs() {
-    let n1 = Number(document.getElementById('num1').value)
-    let n2 = Number(document.getElementById('num2').value)
-    let res = Math.abs(n1)
-    document.getElementById('saida').innerHTML = 'Resultado: ' + res
-}
-
-*/
